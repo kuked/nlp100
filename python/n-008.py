61bb820c3c431b768fbe5048708dcbd1ba400436
donke <aka.kudoken@gmail.com>
2016-01-19 18:50:13 +0900

    Add answer of 08

---
+++ python/n-008.py
@@ -0,0 +1,13 @@
+def cipher(s):
+    def _cipher(c):
+        o = ord(c)
+        if o >= 97 and o <= 122:
+            return chr(219-o)
+        else:
+            return c
+    return ''.join([_cipher(c) for c in s])
+
+if __name__ == '__main__':
+    song = '99 Bottles of Beer.'
+    if not song == cipher(cipher(song)):
+        print('ng')
