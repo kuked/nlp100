06739826d1d4d5976287b5e6a3d4f027b634ca14
donke <aka.kudoken@gmail.com>
2016-01-27 19:05:26 +0900

    Update name of input file

--- python/n-014.sh
+++ python/n-014.sh
@@ -1,5 +1,9 @@
-head -n 3 n-014.py > sh_head.txt
-python n-014.py n-014.py 3 > py_head.txt
+if [ ! -e hightemp.txt ]; then
+    wget -Nq http://www.cl.ecei.tohoku.ac.jp/nlp100/data/hightemp.txt
+fi
+
+head -n 3 hightemp.txt > sh_head.txt
+python n-014.py hightemp.txt 3 > py_head.txt
 
 diff sh_head.txt py_head.txt
 
