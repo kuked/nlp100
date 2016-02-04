527f39ed6c1818d6a0333374b2d85c579b4fcb43
donke <aka.kudoken@gmail.com>
2016-02-04 19:32:31 +0900

    Add answer of 26

---
+++ python/n-026.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import re
+
+multi = re.compile(r'{{基礎情報.*?\n(|.*?\n)*}}', re.MULTILINE)
+kv = re.compile(r'\|(.*?) = (.*)')
+normalize = re.compile(r'\'+(.*?)\'+')
+
+basic_info = {}
+with open('uk.txt', 'r') as r:
+    m = multi.search(r.read())
+    if m:
+        lines = m.group(0).split('\n')
+        for line in lines:
+            if line[0] == '|':
+                m = kv.search(line)
+                basic_info[m.group(1)] = \
+                    normalize.sub(lambda x: x.group(1), m.group(2))
+print(basic_info)
