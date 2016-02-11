96630edb323c1e187d09b2cb77928c5a2fbb3c7e
donke <aka.kudoken@gmail.com>
2016-02-11 17:30:19 +0900

    Add answer of 30

---
+++ python/n-030.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import os
+import requests
+import MeCab
+
+
+if not os.path.exists('neko.txt'):
+    rs = requests.get('http://www.cl.ecei.tohoku.ac.jp/nlp100/data/neko.txt')
+    rs.encoding = 'utf-8'
+    with open('neko.txt', 'w+') as fp:
+        print(rs.text, file=fp)
+
+if not os.path.exists('neko.txt.mecab'):
+    with open('neko.txt.mecab', 'w') as wf:
+        with open('neko.txt') as rf:
+            tagger = MeCab.Tagger('')
+            for line in rf.readlines():
+                print(tagger.parse(line), file=wf, end='')
+
+sentences = []
+with open('neko.txt.mecab') as fp:
+    lines = [line.rstrip() for line in fp.readlines()]
+    tagger = MeCab.Tagger('')
+    chunk = []
+    for line in lines:
+        if line == 'EOS':
+            if len(chunk) > 0:
+                sentences.append(chunk)
+                chunk = []
+        else:
+            # 表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
+            surface, rest = line.split('\t')
+            tagged = rest.split(',')
+            chunk.append({
+                'surface': surface,
+                'base':    tagged[6],
+                'pos':     tagged[0],
+                'pos1':    tagged[1],
+                })
+
+print(sentences)
