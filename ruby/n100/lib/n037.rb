6cf4dc57f8412a943935bf78f44bee6e702ea0f0
kuked <kedoku@gmail.com>
2022-01-16 08:47:57 +0900

    [Ruby] Add test (037)

--- ruby/n100/lib/n037.rb
+++ ruby/n100/lib/n037.rb
@@ -8,13 +8,13 @@ class Array
   end
 
   def take_before(i, span)
-    head = i - span
-    head = head.negative? ? 0 : head
+    head = (i - span).clamp(0, i)
     self[head...i]
   end
 end
 
 class Tagged
+  # word の 前後(span)の語が共起していると仮定する
   def collocate(word, span)
     ls = sentence_include(word)
     ls.map do |ws|
@@ -30,4 +30,22 @@ class Tagged
   def sentence_include(word)
     surfaces.select {|w| w.include? word }
   end
+end
+
+if __FILE__ == $0
+  require 'gr/plot'
+
+  path = 'neko.txt.mecab'
+  tagged = Tagged.new(path)
+
+  collocate = tagged.collocate('猫', 2).flatten
+  top10 = collocate.tally.sort_by {|_, v| -v }[0..9]
+
+  x = top10.map(&:first)
+  y = top10.map(&:last)
+
+  GR.barplot(x, y, ylabel: "collocate with '猫'")
+
+  # Disappears after 5 seconds
+  sleep 5
 end
