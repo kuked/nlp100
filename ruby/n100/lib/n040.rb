92b437d29272224082d614aa3804485606ef9494
kuked <kedoku@gmail.com>
2022-01-17 13:25:22 +0900

    [Ruby] Add test (040)

--- ruby/n100/lib/n040.rb
+++ ruby/n100/lib/n040.rb
@@ -16,3 +16,28 @@ class Morph
     @pos1 = morphs[1]
   end
 end
+
+def read_parsed_by_cabocha(path)
+  open(path) do |f|
+    morphs = []
+    until f.eof?
+      line = f.readline
+      if line =~ /^EOS/
+        yield morphs unless morphs.empty?
+        morphs = []
+      elsif line =~ /^\*/
+        next
+      else
+        morphs << Morph.new(line.chomp)
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  path = 'ai.ja.txt.parsed'
+
+  morphs = []
+  read_parsed_by_cabocha(path) {|v| morphs << v }
+  pp morphs[1]
+end
